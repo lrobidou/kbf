8ac3a1725eea38ff0ef0a5f0736ea179f674c2cb
Lucas Robidou <lucas.robidou@inria.fr>
2021-05-11 17:51:39 +0200

    auto format code

--- cpp-src/FastaReader.h
+++ cpp-src/FastaReader.h
@@ -5,11 +5,12 @@
 #ifndef FASTA_READER_H
 #define FASTA_READER_H
 
-#include <zlib.h>
 #include <stdio.h>
+#include <zlib.h>
+
+#include "fcntl.h"
 #include "kseq.h"
 #include "unistd.h"
-#include "fcntl.h"
 
 #ifndef KSEQ
 // KSEQ_INIT(gzFile, gzread)
@@ -17,31 +18,32 @@ KSEQ_INIT(int, read)
 #endif
 
 class FastaReader {
-    FILE * fp;
+    FILE *fp;
     // int fp; // file handler
     kseq_t *seq;
     int l;
 
-public:
-
-    FastaReader(const char * fname) {
-        fp = fopen(fname, "r"); // TODO: check params
+   public:
+    FastaReader(const char *fname) {
+        fp = fopen(fname, "r");  // TODO: check params
         if (fp == NULL) {
             printf("Could not open file %s\n", fname);
             return;
         }
-        seq = kseq_init( fileno(fp) );
+        seq = kseq_init(fileno(fp));
     }
 
     ~FastaReader() {
         kseq_destroy(seq);
-        fclose( fp );
+        fclose(fp);
     }
 
-    kseq_t * nextSequence() {
+    kseq_t *nextSequence() {
         l = kseq_read(seq);
-        if (l < 0) return NULL;
-        else return seq;
+        if (l < 0)
+            return NULL;
+        else
+            return seq;
     }
 };
 
--- cpp-src/HSNode.hpp
+++ cpp-src/HSNode.hpp
@@ -5,44 +5,42 @@
 #ifndef LIB_HS_NODE
 #define LIB_HS_NODE
 
-#include <unordered_set>
 #include <boost/heap/fibonacci_heap.hpp>
+#include <unordered_set>
 
 typedef uint64_t kmer_t;
 
 class HSNode {
-private:
-  const kmer_t kmer;
-  unsigned degree = 0;
-  unordered_set<unordered_set<kmer_t>*> neighbours;
+   private:
+    const kmer_t kmer;
+    unsigned degree = 0;
+    unordered_set<unordered_set<kmer_t>*> neighbours;
 
-public:
+   public:
+    HSNode(kmer_t kmer) : kmer(kmer) {
+    }
 
-  HSNode(kmer_t kmer) : kmer(kmer){
-  }
+    kmer_t get_kmer() {
+        return kmer;
+    }
 
-  kmer_t get_kmer(){
-    return kmer;
-  }
+    unordered_set<unordered_set<kmer_t>*> get_neighbours() {
+        return neighbours;
+    }
 
-  unordered_set<unordered_set<kmer_t>*> get_neighbours(){
-    return neighbours;
-  }
+    unsigned get_degree() {
+        return degree;
+    }
 
-  unsigned get_degree(){
-    return degree;
-  }
+    void add_neighbour(unordered_set<kmer_t>* N) {
+        neighbours.insert(N);
+        degree++;
+    }
 
-  void add_neighbour(unordered_set<kmer_t>* N){
-    neighbours.insert(N);
-    degree++;
-  }
-
-  void remove_neighbour(unordered_set<kmer_t>* N){
-    neighbours.erase(N);
-    degree--;
-  }
-
+    void remove_neighbour(unordered_set<kmer_t>* N) {
+        neighbours.erase(N);
+        degree--;
+    }
 };
 
 #endif
