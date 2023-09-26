9f43783ffe7dd274a2df710a294a0318eaa6fd8c
Lucas Robidou <lucas.robidou@inria.fr>
2023-09-26 16:26:41 +0200

    changes before changing machine

--- src/main.cpp
+++ src/main.cpp
@@ -25,66 +25,12 @@ using namespace std;
 ////////////////////////////////////////////////////////////////////////////////
 // query a set of test kmers and write out results
 ////////////////////////////////////////////////////////////////////////////////
-void queryKmers(vector<kmer_t>& test_kmers, unordered_set<kmer_t>& true_kmers, BaseBloomFilter& sbf, const string& out_fname) {
+void queryKmers(const vector<kmer_t>& test_kmers, unordered_set<kmer_t>& true_kmers, BaseBloomFilter& sbf, const string& out_fname) {
     vector<bool> states;
-    // time this part
-    auto start = std::chrono::system_clock::now();
     for (auto qk : test_kmers) {
         bool state = sbf.contains(qk);
         states.push_back(state);
     }
-    auto end = std::chrono::system_clock::now();
-    std::chrono::duration<double> elapsed_seconds = end - start;
-    // cerr << "query time: " << elapsed_seconds.count() << " s" << endl;
-    // end the timing here
-
-    // write states and true answers to file
-    // ofstream f_out(out_fname);
-    // f_out << "kmer\tBF_state\ttrue_state" << endl;
-    // for (int i = 0; i < states.size(); i++)
-    //     f_out << test_kmers[i] << "\t" << states[i] << "\t" << (true_kmers.find(test_kmers[i]) != true_kmers.end()) << endl;
-    // f_out.close();
-}
-
-////////////////////////////////////////////////////////////////////////////////
-// Sample a subset of the kmers
-////////////////////////////////////////////////////////////////////////////////
-vector<kmer_t> sample_kmers(unordered_set<kmer_t>& kmer_set, int const set_size, const int K, bool TP = false) {
-    vector<kmer_t> query_kmers;
-
-    // store the kmer set in a vector so that can sample from it
-    vector<kmer_t> kmer_vec;
-    for (auto km : kmer_set) kmer_vec.push_back(km);
-
-    // set up a random number gen
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> sample_dis(0, kmer_set.size() - 1);
-    std::uniform_int_distribution<> shift_dis(0, K - 1);
-    std::uniform_int_distribution<> base_dis(0, 3);
-    const char base_table[4] = {'A', 'C', 'G', 'T'};
-
-    //sample the input kmers
-    int i = query_kmers.size();
-    while (i < set_size) {
-        auto r = sample_dis(gen);
-        assert(r < kmer_set.size());
-        kmer_t sample_kmer = kmer_vec[r];
-        // mutate the sampled kmer
-        if (!TP) {
-            string string_kmer = mer_binary_to_string(sample_kmer, K);
-            auto base = base_table[base_dis(gen)];
-            auto ind = shift_dis(gen);
-            while (string_kmer[ind] == base) {
-                base = base_table[base_dis(gen)];
-            }
-            string_kmer[ind] = base;
-            sample_kmer = mer_string_to_binary(string_kmer.c_str(), K);
-        }
-        query_kmers.push_back(sample_kmer);
-        i++;
-    }
-    return query_kmers;
 }
 
 /////////////////////////////////////////////////////////
@@ -122,7 +68,6 @@ int main(int argc, char* argv[]) {
         else
             assert(TP_string.compare("false") == 0);
     }
-    unordered_set<kmer_t> read_kmers;
 
     // parse input reads -- will build a kmer set from that
     // cerr << "Parsing input fasta " << input_fasta << endl;
@@ -133,7 +78,7 @@ int main(int argc, char* argv[]) {
     // cerr << "Parsing: " << elapsed_seconds.count() << " s" << endl;
     // cerr << "Getting kmers... " << endl;
     // start = std::chrono::system_clock::now();
-    read_kmers = getKmers(reads, K);
+
     // end = std::chrono::system_clock::now();
     // elapsed_seconds = end - start;
     // cerr << "Input kmers: " << read_kmers.size() << endl;
@@ -142,7 +87,8 @@ int main(int argc, char* argv[]) {
     // parse test reads and count the test kmers
     // cerr << "Generating test kmers..." << endl;
     // vector<kmer_t> query_kmers = sample_kmers(read_kmers, query_set_size, K, TP);
-    vector<kmer_t> query_kmers = getKmersVect(parseFasta(queryFilename), K);
+    std::vector<std::string> queryReads = parseFasta(queryFilename);
+
     // const std::vector<size_t> size_factors = {7, 8, 9, 10};  //1 hash function: []
     const std::vector<size_t> size_factors = {3, 5, 7, 9, 15, 18, 21, 24};
     std::cout << "{" << std::endl;
@@ -151,32 +97,35 @@ int main(int argc, char* argv[]) {
         std::cout << "    \"" << size_factor << "\": {" << std::endl;
         std::string prefix = base_prefix + "_" + std::to_string(size_factor);
         {
-            // Test the classic bloom filter
-            // cerr << "#### CLASSIC BLOOM FILTER ####" << endl;
             auto start = std::chrono::high_resolution_clock::now();
+            unordered_set<kmer_t> read_kmers = getKmers(reads, K);
             BaseBloomFilter b(K, read_kmers.size(), size_factor);
             b.populate(read_kmers);
             auto end = std::chrono::high_resolution_clock::now();
 
             cout << "        \"classic_index\": " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "," << endl;
-            start = std::chrono::high_resolution_clock::now();
             cout << "        \"number_of_elements\": " << read_kmers.size() << "," << std::endl;
             cout << "        \"size\": " << read_kmers.size() * size_factor << "," << std::endl;
-            queryKmers(query_kmers, read_kmers, b, prefix + "_classic.txt");
+
+            start = std::chrono::high_resolution_clock::now();
+            queryKmers(getKmersVect(queryReads, K), read_kmers, b, prefix + "_classic.txt");
             end = std::chrono::high_resolution_clock::now();
+
             cout << "        \"classic_query\": " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "," << endl;
         }
 
         {
-            // Test KBF1
-            // cerr << "#### KBF1 ####" << endl;
             auto start = std::chrono::high_resolution_clock::now();
+            unordered_set<kmer_t> read_kmers = getKmers(reads, K);
             KBF1 kbf1(K, read_kmers, size_factor, 1);
             auto end = std::chrono::high_resolution_clock::now();
+
             cout << "        \"kbf1_index\": " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "," << endl;
+
             start = std::chrono::high_resolution_clock::now();
-            queryKmers(query_kmers, read_kmers, kbf1, prefix + "_kbf1.txt");
+            queryKmers(getKmersVect(queryReads, K), read_kmers, kbf1, prefix + "_kbf1.txt");
             end = std::chrono::high_resolution_clock::now();
+
             cout << "        \"kbf1_query\": " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "" << endl;
         }
 
